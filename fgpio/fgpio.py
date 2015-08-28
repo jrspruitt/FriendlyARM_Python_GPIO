22e79667d4d3cb3abee9ff052fdadeee44a95cee
Jason Pruitt <jrspruitt@gmail.com>
2015-08-28 02:22:32 -0700

    Testing branch for interrupts.

--- fgpio/fgpio.py
+++ fgpio/fgpio.py
@@ -26,6 +26,7 @@
 import os
 import mmap
 import struct
+from time import sleep
 
 class GPIO(object):
     """ GPIO gives access to the board gpio pins on the main connector.
@@ -48,9 +49,17 @@ class GPIO(object):
 
         self._type_gpio = 'gpio'
         self._type_pwm = 'pwm'
-        self._type_int = 'eint'
+        self._type_eint = 'eint'
+
 
         self.board = board
+
+        for pin in self.board.pins:
+            self.board.pins[pin]['used'] = False
+
+        self._mm = None
+        self._mem_base_addr = (self.board.MEM_OFFSET & ~(mmap.PAGESIZE-1))
+        self._mem_base_addr_offset = self.board.MEM_OFFSET - self._mem_base_addr
 
         self._sys_pwmchip = '/sys/class/pwm/pwmchip%s' % self.board.PWMCHIP_ID
         self._sys_pwm_export = os.path.join(self._sys_pwmchip, 'export')
@@ -59,18 +68,6 @@ class GPIO(object):
         self._sys_pwm_period = os.path.join(self._sys_pwm, 'period')
         self._sys_pwm_duty_cycle = os.path.join(self._sys_pwm, 'duty_cycle')
         self._sys_pwm_enable = os.path.join(self._sys_pwm, 'enable')
-
-        for pin in self.board.pins:
-            self.board.pins[pin]['used'] = False
-
-        self._base_addr = (self.board.GPIO_OFFSET & ~(mmap.PAGESIZE-1))
-        self._base_addr_offset = self.board.GPIO_OFFSET - self._base_addr
-
-        f = os.open('/dev/mem', os.O_RDWR | os.O_SYNC)
-        self.mm = mmap.mmap(f, self.board.GPIO_LENGTH,
-                            mmap.MAP_SHARED,
-                            mmap.PROT_READ | mmap.PROT_WRITE,
-                            offset=self._base_addr)
 
     def gpio_init(self, pin, direction='in', updown='none'):
         """ Initialize a pin for GPIO use.
@@ -82,6 +79,10 @@ class GPIO(object):
         """
 
         self._pin_available(pin, self._type_gpio)
+
+        if self._mm == None:
+            self._mem_open()
+
         self._gpio_direction(pin, direction)
 
         # Safety check
@@ -101,12 +102,15 @@ class GPIO(object):
 
         self._pin_check(pin, self._type_gpio)
         self._gpio_close(pin)
+        self._mem_close()
 
     def gpio_close_all(self):
         """ Close all GPIO pins."""
 
         for pin in self.board.pins:
             self._gpio_close(pin)
+
+        self._mem_close()
 
     def gpio_read(self, pin):
         """ Read pin value.
@@ -173,7 +177,7 @@ class GPIO(object):
 
         # Error if pin is already exported.
         try:
-            self._pwm_writer(self._sys_pwm_export, self.board.pins[pin][self._type_pwm]['num'])
+            self._sys_write(self._sys_pwm_export, self.board.pins[pin][self._type_pwm]['num'])
         except:
             pass
 
@@ -261,6 +265,70 @@ class GPIO(object):
         self._pin_check(pin, self._type_pwm)
         self._pwm_enable(pin, 0)
 
+
+
+    def eint_init(self, pin, trigger, func=None):
+        """ Configure interrupt pin.
+
+        Arguments:
+            pin:Int         Pin number of board connector.
+            trigger:Str     Trigger low, high, rising, falling, or both
+            func:func       Callback function.
+        """
+        self._pin_available(pin, self._type_eint)
+
+        if self._mm == None:
+            self._mem_open()
+
+        self._gpio_function(pin, self.board.FUNC_EINT)
+
+        if trigger not in ['low', 'high', 'rising', 'falling', 'none', 'both']:
+            self._value_error('Trigger must be low,  high, rising, falling, none, or both.')
+
+        self._eint_trigger(pin, trigger)
+        self._eint_clear_event(pin)
+        self.board.pins[pin]['used'] = self._type_eint
+ 
+        while True:
+            event = self._eint_get_event(pin)
+            print 'event', event
+            if event:
+                self._eint_clear_event(pin)
+                break
+            sleep(.01)
+        self._eint_close(pin)
+
+    def eint_close(self, pin):
+        """Close interrupt pin.
+
+        Arguments:
+            Pin:Int     Pint number of the board connector.
+        """
+        self._pin_check(pin, self._type_eint)
+        self._eint_close(pin)
+        self._mem_close()
+
+    def eint_close_all(self):
+        """ Close all EINT pins."""
+
+        for pin in self.board.pins:
+            self._eint_close(pin)
+
+        self._mem_close()
+
+    def eint_get_event(pin):
+        """Poll for event trigger.
+
+        Returns:Int 0 for False 1 for True.
+        """
+        self._pin_check(pin, self._type_eint)
+        return self._eint_get_event(pin)
+
+    def eint_clear_event(pin):
+        """Clear triggered event."""
+        self._pin_check(pin, self._type_eint)
+        self._eint_clear_event(pin)
+
     def _exception(self, msg):
         raise Exception('fgpio: %s' % msg)
 
@@ -292,14 +360,14 @@ class GPIO(object):
             self._exception('pin %s already in use as %s.' % (pin, self.board.pins[pin]['used']))
 
     def _gpio_close(self, pin):
-        if self.board.pins[pin]['used'] == self._type_gpio:
+        if self.board.pins[pin]['used'] in [self._type_gpio, self._type_eint]:
             self._gpio_write(pin, self.board.DATA_RESET)
             self._gpio_function(pin, self.board.FUNC_RESET)
             self._gpio_updn(pin, self.board.UPDN_RESET)
             self.board.pins[pin]['used'] = False
 
     def _gpio_read(self, pin):
-        self._mem_seek_addr(pin, self.board.GPIO_DATA_OFFSET)
+        self._gpio_mem_seek_addr(pin, self.board.GPIO_DATA_OFFSET)
         data = self._mem_read()
         return 0x01 & (data >> self.board.pins[pin][self._type_gpio]['num'])
 
@@ -309,8 +377,8 @@ class GPIO(object):
         else:
             value =  0
 
-        self._mem_seek_addr(pin, self.board.GPIO_DATA_OFFSET)
-        self._mem_write(pin, value)
+        self._gpio_mem_seek_addr(pin, self.board.GPIO_DATA_OFFSET)
+        self._gpio_mem_write(pin, value)
 
     def _gpio_direction(self, pin, direction):
         direction = direction.lower()
@@ -318,18 +386,14 @@ class GPIO(object):
             func_num = self.board.FUNC_OUT
         elif direction == 'in':
             func_num = self.board.FUNC_IN
-        elif direction == 'pwm':
-            func_num = self.board.FUNC_PWM
-        elif direction == 'eint':
-            func_num = self.board.FUNC_EINT
         else:
             self._value_error('Bad GPIO func: %s' % direction)
 
         self._gpio_function(pin, func_num)
 
     def _gpio_function(self, pin, func):
-        self._mem_seek_addr(pin, self.board.GPIO_CON_OFFSET)
-        self._mem_write2(pin, func)
+        self._gpio_mem_seek_addr(pin, self.board.GPIO_CON_OFFSET)
+        self._gpio_mem_write2(pin, func)
 
     def _gpio_updown(self, pin, updown):
         updown = updown.lower()
@@ -345,29 +409,24 @@ class GPIO(object):
         self._gpio_updn(pin, updn)
 
     def _gpio_updn(self, pin, updn):
-        self._mem_seek_addr(pin, self.board.GPIO_UPD_OFFSET)
-        self._mem_write2(pin, updn)
+        self._gpio_mem_seek_addr(pin, self.board.GPIO_UPD_OFFSET)
+        self._gpio_mem_write2(pin, updn)
 
-    def _mem_seek_addr(self, pin, offset_bank):
+    def _gpio_mem_seek_addr(self, pin, offset_bank):
         bank = self.board.pins[pin]['bank']
-        self.mm.seek(self._base_addr_offset + self.board.banks[bank] + offset_bank)
+        self._mm.seek(self._mem_base_addr_offset + self.board.banks[bank] + offset_bank)
 
-    def _mem_write(self, pin, value):
+    def _gpio_mem_write(self, pin, value):
         pin_num = self.board.pins[pin][self._type_gpio]['num']
         data = self._mem_read()
         data = (data & ~(1 << (pin_num))) | ((value & 1) << (pin_num))
-        self.mm.write(struct.pack('I', data))
+        self._mem_write(data)
 
-    def _mem_write2(self, pin, value):
+    def _gpio_mem_write2(self, pin, value):
         pin_num = self.board.pins[pin][self._type_gpio]['num']
         data = self._mem_read()
         data = (data & ~(3 << (pin_num * 2))) | ((value & 3) << (pin_num * 2))
-        self.mm.write(struct.pack('I', data))
-
-    def _mem_read(self):
-        ret = struct.unpack('I',  self.mm.read(4))[0]
-        self.mm.seek(self.mm.tell()-4)
-        return ret
+        self._mem_write(data)
 
     def _pwm_close(self, pin):
         if self.board.pins[pin]['used'] == self._type_pwm:
@@ -376,7 +435,7 @@ class GPIO(object):
             try:
                 self._pwm_duty_cycle(pin, 0)
                 self._pwm_enable(pin, 0)
-                self._pwm_writer(self._sys_pwm_unexport, self.board.pins[pin][self._type_pwm]['num'])
+                self._sys_write(self._sys_pwm_unexport, self.board.pins[pin][self._type_pwm]['num'])
             except:
                 pass
 
@@ -386,7 +445,7 @@ class GPIO(object):
         if not os.path.exists(path):
             self._exception('PWM pin %s period path does not exist.' % pin)
 
-        return self._pwm_reader(path)
+        return self._sys_read(path)
 
     def _pwm_period(self, pin, period):
         self._int_check(period, 'period')
@@ -399,7 +458,7 @@ class GPIO(object):
         if not os.path.exists(path):
             self._exception('PWM pin %s period path does not exist.' % pin)
 
-        self._pwm_writer(path, period)
+        self._sys_write(path, period)
 
     def _pwm_get_duty_cycle(self, pin):
         path = self._sys_pwm_duty_cycle % self.board.pins[pin][self._type_pwm]['num']
@@ -407,7 +466,7 @@ class GPIO(object):
         if not os.path.exists(path):
             self._exception('PWM pin %s duty cycle path does not exist.' % pin)
 
-        return self._pwm_reader(path)
+        return self._sys_read(path)
 
     def _pwm_duty_cycle(self, pin, duty_cycle):
         self._int_check(duty_cycle, 'duty cycle')
@@ -420,7 +479,7 @@ class GPIO(object):
         if not os.path.exists(path):
             self._exception('PWM pin %s duty cycle path does not exist.' % pin)
 
-        self._pwm_writer(path, duty_cycle)
+        self._sys_write(path, duty_cycle)
 
     def _pwm_enable(self, pin, onoff):
         path = self._sys_pwm_enable % self.board.pins[pin][self._type_pwm]['num']
@@ -428,18 +487,84 @@ class GPIO(object):
         if not os.path.exists(path):
             self._exception('PWM pin %s enable path does not exist.' % pin)
 
-        self._pwm_writer(path, onoff)
+        self._sys_write(path, onoff)
 
-    def _pwm_writer(self, path, value):
+    def _eint_close(self, pin):
+        self._eint_control(pin, self.board.EINT_RESET)
+        self._gpio_close(pin)
+
+    def _eint_trigger(self, pin, trigger):
+        if trigger == 'low':
+            trig_num = self.board.EINT_LOW
+        elif trigger == 'high':
+            trig_num = self.board.EINT_HIGH
+        elif trigger == 'rising':
+            trig_num = self.board.EINT_RISE
+        elif trigger == 'falling':
+            trig_num = self.board.EINT_FALL
+        elif trigger == 'both':
+            trig_num = self.board.EINT_BOTH
+        else:
+            self._value_error('Trigger must be low, high, rising, falling, or both.')
+
+        self._eint_control(pin, trig_num)
+
+    def _eint_control(self, pin, value):
+        pin_num = self.board.pins[pin][self._type_eint]['num']
+        self._eint_mem_seek_addr(self.board.EINT_CONT_OFFSET + (((pin_num * 4) / 32))*4)
+        data = self._mem_read()
+        pin_offset = ((pin_num * 4) % 32)
+        data = (data & ~(7 << pin_offset)) | ((value & 7) << pin_offset)
+        self._mem_write(data)
+
+    def _eint_get_event(self, pin):
+        self._eint_mem_seek_addr(self.board.EINT_PEND_OFFSET)
+        data = self._mem_read()
+        return 0x01 & (data >> self.board.pins[pin][self._type_eint]['num'])
+
+    def _eint_clear_event(self, pin):
+        self._eint_mem_seek_addr(self.board.EINT_PEND_OFFSET)
+        data = self._mem_read()
+        pin_num = self.board.pins[pin][self._type_eint]['num'] 
+        data = (data & ~(1 << pin_num)) | (1 << pin_num)
+        self._mem_write(data)
+
+    def _eint_mem_seek_addr(self, offset):
+        self._mm.seek(self._mem_base_addr_offset + offset)
+
+    def _mem_open(self):
+        f = os.open('/dev/mem', os.O_RDWR | os.O_SYNC)
+        self._mm = mmap.mmap(f, self.board.MEM_LENGTH,
+                            mmap.MAP_SHARED,
+                            mmap.PROT_READ | mmap.PROT_WRITE,
+                            offset=self._mem_base_addr)
+
+    def _mem_close(self):
+        for pin in self.board.pins:
+            if self.board.pins[pin]['used'] in [self._type_gpio, self._type_eint]:
+                break
+        else:
+            self._mm.close()
+            self._mm = None
+
+    def _mem_read(self):
+        ret = struct.unpack('I',  self._mm.read(4))[0]
+        self._mm.seek(self._mm.tell()-4)
+        return ret
+
+    def _mem_write(self, data):
+        self._mm.write(struct.pack('I', data))
+
+    def _sys_write(self, path, value):
         if not os.path.exists(path):
-            self._exception('PWM sys file does not exist. %s' % path)
+            self._exception('Sys file does not exist. %s' % path)
 
         with open(path, 'w') as f:
             f.write('%s' % value)
 
-    def _pwm_reader(self, path):
+    def _sys_read(self, path):
         if not os.path.exists(path):
-            self._exception('PWM sys file does not exist. %s' % path)
+            self._exception('Sys file does not exist. %s' % path)
 
         ret = -1
         with open(path, 'r') as f:
